1f1dd23a2e5b0efe9131bdeb0387e486df2997f4
Estela Miranda Batista <estelambatista@gmail.com>
2020-04-28 16:22:18 -0300

    Iniciando Portifolio

---
+++ js/init.js
@@ -0,0 +1,14 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $(document).ready(function(){
+    $('.materialboxed').materialbox();
+    });
+    $(document).ready(function() {
+    M.updateTextFields();
+    });
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
