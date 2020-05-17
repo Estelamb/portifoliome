3098d5f10ce88c6d8c7f21d17d7d0f2e371585f2
Estela Miranda Batista <estelambatista@gmail.com>
2020-05-17 11:18:26 -0300

    Atualizando Sobre

--- js/init.js
+++ js/init.js
@@ -10,5 +10,20 @@
     M.updateTextFields();
     });
 
+    function typeWrite(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ';
+    textoArray.forEach(function(letra, i){
+
+    setTimeout(function(){
+        elemento.innerHTML += letra;
+    }, 75 * i)
+
+  });
+}
+
+ const titulo = document.querySelector('.titulo-principal');
+ typeWrite(titulo);
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
