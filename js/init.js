7f8eaa96d521f868f14fb581373ee0dc5ee1e9bc
Estela Miranda Batista <estelambatista@gmail.com>
2020-05-17 15:09:47 -0300

    Adicionando Botão Flutuante

--- js/init.js
+++ js/init.js
@@ -25,5 +25,16 @@
  const titulo = document.querySelector('.titulo-principal');
  typeWrite(titulo);
 
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.fixed-action-btn').floatingActionButton();
+  });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
